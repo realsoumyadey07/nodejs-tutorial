f2f6a01b0900a61869262deeb3528aa813dd34e7
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-03-20 21:53:58 +0530

    server created

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import express from 'express';
+const app = express();
 
 
+app.get('/',(req, res)=>{
+    res.send("welcome from server");
+})
+app.get('/greencoconut',(req, res)=>{
+    res.send("Hi this is soumya, and I love coconut");
+})
+
+app.listen(8000,()=>{
+    console.log('server is running on 8000');
+})
