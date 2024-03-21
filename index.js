1ab414c2ceff7e71f72b47dd74e90ad4b22556f9
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-03-21 10:50:11 +0530

    on going

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/',(req, res)=>{
 app.get('/greencoconut',(req, res)=>{
     res.send("Hi this is soumya, and I love coconut");
 })
+app.post('/item', (req, res)=>{
+    res.json('data is saved');
+})
 
 app.listen(8000,()=>{
     console.log('server is running on 8000');
